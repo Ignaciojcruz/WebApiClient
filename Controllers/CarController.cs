c457ed40200eb8d405051568e5c4e88859ca0600
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-11-23 15:53:31 -0300

    Add CRUD for Car

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -14,35 +14,127 @@ namespace WebClientWebApi2.Controllers
 {
     public class CarController : Controller
     {
-        string Baseurl = "http://localhost:62451/";
-        // GET: Car
+                
         public async Task<ActionResult> Index()
         {
             CarDAL carDAL = new CarDAL();
             List<Car> cars = new List<Car>();
 
             cars = await carDAL.GetCars();
-            //List<Car> cars = new List<Car>();
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = new Uri(Baseurl);
-            //    client.DefaultRequestHeaders.Clear();
-
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //    HttpResponseMessage Res = await client.GetAsync("api/Car");
-
-            //    if (Res.IsSuccessStatusCode)
-            //    {
-            //        var CarResponse = Res.Content.ReadAsStringAsync().Result;
-            //        cars = JsonConvert.DeserializeObject<List<Car>>(CarResponse);
-            //    }
-
-            //}
-
+            
             return View(cars);
         }
 
-        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                CarDAL carDAL = new CarDAL();
+                Car car = new Car();
+
+                car.Id = Convert.ToInt32(collection["Id"]);
+                car.Brand = collection["Brand"].ToString();
+                car.Model = collection["Model"].ToString();
+                car.Year = Convert.ToInt32(collection["Year"]);
+                car.Type = collection["Type"].ToString();
+                car.IsDeleted = Convert.ToBoolean(collection["IsDeleted"]);
+
+                int resp = await carDAL.SetCar(car);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            CarDAL carDAL = new CarDAL();
+            Car car = new Car();
+
+            car = await carDAL.GetCar(id);
+
+            return View(car);
+        }
+
+        // POST: Prueba/Edit/5
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                CarDAL carDAL = new CarDAL();
+                Car car = new Car();
+
+                car.Id = Convert.ToInt32(collection["Id"]);
+                car.Brand = collection["Brand"].ToString();
+                car.Model = collection["Model"].ToString();
+                car.Year = Convert.ToInt32(collection["Year"]);
+                car.Type = collection["Type"].ToString();
+                car.IsDeleted = Convert.ToBoolean(collection["IsDeleted"]);
+
+                int resp = await carDAL.SetCar(car);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            CarDAL carDAL = new CarDAL();
+            Car car = new Car();
+
+            car = await carDAL.GetCar(id);
+
+            return View(car);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            CarDAL carDAL = new CarDAL();
+            Car car = new Car();
+
+            car = await carDAL.GetCar(id);
+
+            return View(car);
+        }
+
+        // POST: Prueba/Delete/5
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                CarDAL carDAL = new CarDAL();
+                Car car = new Car();
+
+                car.Id = id;
+                car.IsDeleted = true;
+
+                int resp = await carDAL.SetCar(car);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 
 }
--- DAL/CarDAL.cs
+++ DAL/CarDAL.cs
@@ -29,9 +29,59 @@ namespace WebClientWebApi2.DAL
                     var CarResponse = Res.Content.ReadAsStringAsync().Result;
                     cars = JsonConvert.DeserializeObject<List<Car>>(CarResponse);
                 }
-
             }
             return cars;
+        }
+
+        public async Task<Car> GetCar(int id)
+        {
+            Car car = new Car();
+            car.Id = id;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                                               
+                HttpResponseMessage res = await client.GetAsync("api/Car?id=" + id.ToString());
+                                                
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var CarResponse = res.Content.ReadAsStringAsync().Result;
+                    car = JsonConvert.DeserializeObject<Car>(CarResponse);
+                }
+            }
+            return car;
+        }
+
+        public async Task<int> SetCar(Car car)
+        {
+            int resp = 0;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                               
+                var json = JsonConvert.SerializeObject(car);
+                var content = new StringContent(json);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, Baseurl + "api/Car");                
+                request.Content = content;
+
+                HttpResponseMessage res = await client.SendAsync(request);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var carResponse = res.Content.ReadAsStringAsync().Result;
+                    resp = 1;
+                }
+                
+            }
+
+            return resp;
         }
     }
 }
